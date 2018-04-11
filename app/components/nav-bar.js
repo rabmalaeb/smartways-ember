3a4aa3baf7a1f803d23876b7845cfc48aeeacc8b
Omar Hamdan <omarhamdan1993@gmail.com>
2018-04-11 22:52:14 +0300

    implement scroll

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,7 +1,17 @@
 import Component from '@ember/component';
+import $ from 'jquery';
 
 export default Component.extend({
   tagName: 'ul',
 
-  classNames: ['nav-bar']
-})
+  classNames: ['nav-bar'],
+
+  actions: {
+    scroll(id) {
+      $('html').animate({
+        scrollTop: $(`#${id}`).offset().top
+      }, 1000);
+      return true;
+    }
+  }
+});
