a97aa739e57196a46acaec42b233d3b0a0149aab
Omar Hamdan <omarhamdan1993@gmail.com>
2018-04-11 23:33:38 +0300

    address rabih's review

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,17 +1,6 @@
 import Component from '@ember/component';
-import $ from 'jquery';
 
 export default Component.extend({
   tagName: 'ul',
-
-  classNames: ['nav-bar'],
-
-  actions: {
-    scroll(id) {
-      $('html').animate({
-        scrollTop: $(`#${id}`).offset().top
-      }, 1000);
-      return true;
-    }
-  }
+  classNames: ['nav-bar']
 });
